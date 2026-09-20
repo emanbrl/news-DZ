ed432f2ed1b4ca0e89a2dc4701a4b276b337752a
Imene Bouretal <im97en@gmail.com>
2026-09-20 22:53:48 +0100

    Refactor APS article parsing into function

--- scraper.py
+++ scraper.py
@@ -5,15 +5,44 @@ BASE_URL = "https://www.aps.dz"
 SOURCE_URL = f"{BASE_URL}/en"
 
 # =========================================
+# Parse on article
+# =========================================
+
+def parse_article(article_url):
+    article_response = requests.get(article_url)
+
+    # Parse homepage HTML
+    article_soup = BeautifulSoup(
+        article_response.text,
+        "html.parser"
+    )
+
+    title = article_soup.find("h1")
+
+    date = article_soup.find("span", class_="text-xs")
+
+    lead = article_soup.find("p")
+
+    description = lead.get_text(" ", strip=True) if lead else None
+
+    article = {
+        "title": title.get_text(strip=True) if title else None,
+        "url": article_url,
+        "date": date.get_text(strip=True) if date else None,
+        "description": description
+    }
+
+    return article
+
+
+# =========================================
 # Fetch homepage
 # =========================================
 
 response = requests.get(SOURCE_URL)
 print(response.status_code)
 
-# Parse homepage HTML
 soup = BeautifulSoup(response.text, "html.parser")
-
 
 # =========================================
 # Find article URLs
@@ -32,35 +61,18 @@ for link in soup.find_all("a"):
 
 print("Found", len(article_urls), "unique articles")
 
+
+# =========================================
+# Parse articles
+# =========================================
+
 articles = []
 
 for article_path in article_urls:
 
     article_url = BASE_URL + article_path
 
-    article_response = requests.get(article_url)
-
-    article_soup = BeautifulSoup(
-        article_response.text,
-        "html.parser"
-    )
-
-    title = article_soup.find("h1")
-
-    date = article_soup.find("span", class_="text-xs")
-
-    lead = article_soup.find(
-        string=lambda text: text and text.startswith("ALGIERS -")
-    )
-
-    description = lead.parent.get_text(strip=True) if lead else None
-
-    article = {
-        "title": title.get_text(strip=True) if title else None,
-        "url": article_url,
-        "date": date.get_text(strip=True) if date else None,
-        "description": description,
-    }
+    article = parse_article(article_url)
 
     articles.append(article)
 
